c6be5d31485b79c97cd8cef0803208530e534048
gporras <gporras@bancolombia.com.co>
2023-08-14 18:33:35 -0500

    UUID as identifiers

---
+++ src/mooc/main/com/app/courses/domain/CourseId.java
@@ -0,0 +1,12 @@
+package com.app.courses.domain;
+import com.app.domain.Identifier;
+
+import java.util.UUID;
+
+public class CourseId extends Identifier {
+
+
+    public CourseId(String value) {
+        super(value);
+    }
+}
